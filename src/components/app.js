440992e154f3652d7c30e0119a2f16602583a0fb
Jet Shoji <jetshoji@Jets-MacBook-Pro.local>
2017-07-09 19:18:28 -0400

    added second element in app.js

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>Jet is light weight </div>
+      <div>
+      	<div>Jet is light weight </div>
+      	<div>Paul is Russian </div>
+      </div>
     );
   }
 }
