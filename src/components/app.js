f5dd855c7db8ec470c926be18f1ef26a88598b49
Jet Shoji <jetshoji@Jets-MacBook-Pro.local>
2017-07-17 16:34:30 -0400

    changed Home -> home bc it was throwing errors in heroku

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Navi from './navi/Navi'; 
+import Navi from './Navi/navi'; 
 import Helmet from 'react-helmet';
 
 export default class App extends Component {
